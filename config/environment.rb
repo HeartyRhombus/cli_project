d1fbc637ff37fd845d5ebac80122e26626893eb1
HeartyRhombus <kaylahmg@gmail.com>
2020-09-28 11:27:39 -0700

    add menu beginning in cli file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 require 'pry'
-require 'HTTParty'
+require 'httparty'
 require 'json'
 
 require_relative '../lib/api'
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,20 @@
 class CLI
 
-    def run
+    def main_menu
+        puts ""
+        puts "----------------------------------"
         puts "Welcome to my Ghibli Movie Library"
+        puts "----------------------------------"
+        puts ""
+        puts ""
+        puts "To view a listing of Ghibli movies please select from the following options:"
+        puts "To view movies by title, enter 'title'."
+        puts "To view by director, enter 'director'."
+        puts "To view by release date, enter 'date'."
+        puts "To exit, enter 'exit'."
+        puts ""
+        input = gets.strip.downcase
+
     end
 
 end
