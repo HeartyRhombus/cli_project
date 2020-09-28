8ea16bed335b08f9cad072a8b724b4fed545f5f1
HeartyRhombus <kaylahmg@gmail.com>
2020-09-28 14:22:42 -0700

    add movie index

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,29 +4,44 @@ class CLI
 
         API.get_films
 
+        input = ''
+
         puts ""
         puts "----------------------------------"
         puts "Welcome to the Ghibli Movie Library"
         puts "----------------------------------"
         puts ""
-        puts ""
-        puts "To view a listing of Ghibli movies please enter 'titles'."
-        puts ""
-        input = gets.strip.downcase
-        # while input != "exit"
-        #     if input == "titles"
-        #         #puts listing of titles
-        #     else
-        #         nil
-        #     end
-        # end
+        
+        while input != "exit"
 
+            puts ""
+            puts "To view a listing of Ghibli movies please enter 'titles'."
+            puts ""
+
+            input = gets.strip.downcase
+
+    
+                case input
+                    when "titles"
+                        # binding.pry
+                        titles
+                    else
+                        nil
+                end
+            end
         
         # puts "To view a title's description, enter 'description'."
         # puts "To view a title's director, enter 'director'."
         # puts "To view a title's release date, enter 'date'."
         # puts "To exit, enter 'exit'."
 
+    end
+
+
+    def titles
+        Film_Title.all.sort_by {|film| film.title}.each.with_index(1) do |film, index|
+            puts "#{index}. #{film.title}"
+        end
     end
 
 end
