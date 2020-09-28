f8698600db353fd925162239d7fe6609dfd272b5
HeartyRhombus <kaylahmg@gmail.com>
2020-09-28 11:17:03 -0700

    config folder rename

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require 'HTTParty'
+require 'json'
+
+require_relative '../lib/api'
+require_relative '../lib/cli'
+require_relative '../lib/director'
+require_relative '../lib/film_title'
+require_relative '../lib/release_date'
---
+++ lib/cli.rb
@@ -0,0 +1,7 @@
+class CLI
+
+    def run
+        puts "Welcome to my Ghibli Movie Library"
+    end
+
+end
